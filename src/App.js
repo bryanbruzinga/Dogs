b2b6bd00537773c131c06628a7dc9ace970c97de
Bryan da Silva Bruzinga <bryanbruzinga1990@gmail.com>
2021-03-12 11:07:53 -0300

    criado arquivos bases e reset de estilos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Header from './Components/Header.js'
+import Footer from './Components/Footer.js'
+import Home from './Components/Home.js'
+import Login from './Components/Login/Login';
+
 function App() {
   return (
     <div>
-      Olá
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
