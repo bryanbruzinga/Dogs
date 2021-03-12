00f232c5b81478cc7a02ce9efce67200d177304a
Bryan da Silva Bruzinga <bryanbruzinga1990@gmail.com>
2021-03-12 15:54:03 -0300

    criação de custom hook de form e criado processo de autenticação

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,29 +1,33 @@
 import Button from "Components/Forms/Button";
 import Input from "Components/Forms/Input";
+import useForm from "../../Hooks/useForm";
 import React from "react";
 import { Link } from "react-router-dom";
 
 const LoginForm = () => {
-  const [username, setUsername] = React.useState("");
-  const [password, setPassword] = React.useState("");
+  const username = useForm();
+  const password = useForm();
 
   function handleSubmit(event) {
     event.preventDefault();
-    fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ username, password }),
-    });
+
+    if (username.validate() && password.validate()) {
+      fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({}),
+      });
+    }
   }
 
   return (
     <div>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <Input label="Usuário" type="text" name="username" />
-        <Input label="Senha" type="password" name="password" />
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
         <Button>Entrar</Button>
       </form>
       <Link to="/login/criar">Cadastro</Link>
