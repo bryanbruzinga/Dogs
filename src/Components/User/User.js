dd6caa34212a5ba934ea6ce62e92de1c0d5b393e
Bryan da Silva Bruzinga <bryanbruzinga1990@gmail.com>
2021-03-15 15:37:02 -0300

    criação de página de erro 404

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,4 +1,5 @@
 import Feed from "Components/Feed/Feed";
+import NotFound from "Components/NotFound";
 import React from "react";
 import { Route, Routes } from "react-router";
 import { UserContext } from "UserContext";
@@ -16,6 +17,7 @@ const User = () => {
         <Route path="/" element={<Feed user={data.id} />} />
         <Route path="postar" element={<UserPhotoPost />} />
         <Route path="estatisticas" element={<UserStats />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </section>
   );
