280e4aaae83da47814e2c9c0834ffbc90b166f28
Bryan da Silva Bruzinga <bryanbruzinga1990@gmail.com>
2021-03-16 10:19:00 -0300

    incluído mensagem caso o usuário não tenha postagem

--- src/Components/User/UserStatsGraphs.js
+++ src/Components/User/UserStatsGraphs.js
@@ -28,29 +28,33 @@ const UserStatsGraphs = ({ data }) => {
       <div className={`${styles.total} ${styles.graphItem}`}>
         <p>Acessos: {total}</p>
       </div>
-      <div className={styles.graphItem}>
-        <VictoryPie
-          data={graph}
-          innerRadius={50}
-          padding={{ top: 20, bottom: 20, left: 80, right: 80 }}
-          style={{
-            data: {
-              fillOpacity: 0.9,
-              stroke: "#fff",
-              strokeWidth: 2,
-            },
-            labels: {
-              fontSize: 14,
-              fill: "#333",
-            },
-          }}
-        />
+      {data.length <= 0 ? (
+        <p>Poste fotos para ver as estatísticas.</p>
+      ) : (
         <div className={styles.graphItem}>
-          <VictoryChart>
-            <VictoryBar alignment="start" data={graph}></VictoryBar>
-          </VictoryChart>
+          <VictoryPie
+            data={graph}
+            innerRadius={50}
+            padding={{ top: 20, bottom: 20, left: 80, right: 80 }}
+            style={{
+              data: {
+                fillOpacity: 0.9,
+                stroke: "#fff",
+                strokeWidth: 2,
+              },
+              labels: {
+                fontSize: 14,
+                fill: "#333",
+              },
+            }}
+          />
+          <div className={styles.graphItem}>
+            <VictoryChart>
+              <VictoryBar alignment="start" data={graph}></VictoryBar>
+            </VictoryChart>
+          </div>
         </div>
-      </div>
+      )}
     </section>
   );
 };
